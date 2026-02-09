3b26443f59fe07891284f28a8107065cd3097186
yunus-17 <mohammedyunus7010@gmail.com>
2026-02-09 14:05:12 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-const hi = "hello yunus!!!"
-console.log(hi)
+const express = require("express");
+
+const app = express();
+
+// Middleware (optional but good practice)
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+    res.send("Hello World! 🚀 Express Server is running");
+});
+
+// Another sample route
+app.get("/about", (req, res) => {
+    res.json({
+        message: "This is the About route",
+        status: "Success"
+    });
+});
+
+// Define Port
+const PORT = 3000;
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
